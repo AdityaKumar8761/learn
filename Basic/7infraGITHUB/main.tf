603eee782cf0217631a8aee28f35cfd2f9ad3007
AdityaKumar8761 <S24CSEU1905@bennett.edu.in>
2026-04-17 23:22:42 +0530

    date

--- Basic/7infraGITHUB/main.tf
+++ Basic/7infraGITHUB/main.tf
@@ -1,8 +1,12 @@
-resource "github_repository" "terraform-first" {
+resource "github_repository" "terraform_first" {
   name        = "first-repo-from-terraform"
   description = "Myfirst resource for terraform IAC"
 
   visibility = "public"
 
   auto_init = true
+}
+
+output "terraform-first-repo" {
+  value = github_repository.terraform_first.html_url
 }
